1b6115edea3f888f094e675bab2d73ea4975fc4a
KonstantinVlasov <konstantinnvlasov@gmail.com>
2015-04-12 21:38:20 +0300

    config.php for php5.3

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,17 @@
 <?php
 
-$config = (object) ['application' => (object)[
+//$config = (object) ['application' => (object)[
+//  'apiUri' => 'localhost',
+//  'baseUri' => '/booking-web/public/',
+//  'siteName' => 'booking',
+//  'translations' => '/../app/translations'
+//]];
+
+$config = (object) array('application' => (object) array(
   'apiUri' => 'localhost',
   'baseUri' => '/booking-web/public/',
   'siteName' => 'booking',
   'translations' => '/../app/translations'
-]];
+));
 
 return $config;
